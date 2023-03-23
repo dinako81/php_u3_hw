9d7d0d87a6a203c839acad5e8ae9b22d0a0a7e10
dinako81 <dina.koliata@gmail.com>
2023-03-23 09:47:43 +0200

    update

---
+++ App/DB/App.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+
+class App {
+
+    public static function process()
+    {
+
+// ipaistinti
+
+    }
+// routeris gauna masyva su url
+
+// If sveitaine, eini is vetaine, if virtuve eini i virtuve
+// ziurim visada metoda
+
+
+    private static function router(array $url) 
+    {
+//  ipaistinti
+
+
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// patikrinti ar teisingas kelias?
+
+echo App::process();
+
+//papildyti namespace
